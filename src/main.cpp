254825ea180bceb4963c134b9c8bd0b6edc77fcc
Denis Tregubov <denis_tregubov@epam.com>
2019-11-08 08:32:25 +0300

    fade in/out added

--- src/main.cpp
+++ src/main.cpp
@@ -1,26 +1,48 @@
 #include <Arduino.h>
 #include <LowPower.h>
 
-#define PIN 7
+#define LED_PIN 7
 #define IR 15
+
+void led_fade_in();
+void led_fade_out();
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
-  pinMode(PIN, OUTPUT);
+  pinMode(LED_PIN, OUTPUT);
   pinMode(IR, INPUT);
-
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   int ir_distance = analogRead(IR);
   if (ir_distance > 30) { // Open
-    digitalWrite(PIN, HIGH);
+    led_fade_in();
   } else  { // Closed
-    digitalWrite(PIN, LOW);
+    led_fade_out();
   }
   LowPower.idle(SLEEP_1S, ADC_OFF, TIMER2_OFF, TIMER1_OFF, TIMER0_OFF, 
                 SPI_OFF, USART0_OFF, TWI_OFF);
 
+}
+
+void led_fade_in() {
+  // fade in from min to max in increments of 5 points:
+  for (int fadeValue = 0 ; fadeValue <= 255; fadeValue += 5) {
+    // sets the value (range from 0 to 255):
+    analogWrite(LED_PIN, fadeValue);
+    // wait for 30 milliseconds to see the dimming effect
+    delay(30);
+  }
+}
+
+void led_fade_out() {
+  // fade out from max to min in increments of 5 points:
+  for (int fadeValue = 255 ; fadeValue >= 0; fadeValue -= 5) {
+    // sets the value (range from 0 to 255):
+    analogWrite(LED_PIN, fadeValue);
+    // wait for 30 milliseconds to see the dimming effect
+    delay(30);
+  }
 }
